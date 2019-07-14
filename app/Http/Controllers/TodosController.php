d2e241f91064931bb83959f6ce8e92f669b9f3d5
vandna kapoor <vandnakapoor19@gmail.com>
2019-07-13 21:12:59 -0400

    make a model to show all todos with bootstap

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodosController extends Controller
 {
     public function index(){
-        return view('todos.index');
+        $todos = Todo::all();
+        return view('todos.index')->with('todos',$todos);
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,8 +2,28 @@
     <html>
         <head>
         <meta charset="UTF8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
         </head> 
         <body>
-            <h4>TODOS</h4>
+        <div class="container">
+            <h1 class="text-center my-5">TODOS</h1>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card card-default">
+                        <div class="card-header">
+                            Todos
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                @foreach($todos as $data )
+                                    <li class="list-group-item">{{$data->name}}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
         </body>
     </html>
